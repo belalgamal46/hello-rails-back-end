a0dc1da4ee831b150c603122366f402ebda3b88e
theabroadwolf <belalgamal00@gmail.com>
2023-09-01 18:27:04 +0300

    Handle routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def handle_routing_error
+    render json: { errorMessage: 'URL provided is not found please check your URL', status: 'failed' }, status: :not_found
+  end
 end
