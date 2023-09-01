7e018c16f840e5a84dd13df98b3bb7314637f503
theabroadwolf <belalgamal00@gmail.com>
2023-09-01 15:26:30 +0300

    Add greeting messages route in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'greeting-messages', to: 'greeting_messages#index'
 end
