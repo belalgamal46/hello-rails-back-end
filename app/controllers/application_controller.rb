3611b8b6f7c84684622b9f676b2ff21b8a765d14
theabroadwolf <belalgamal00@gmail.com>
2023-09-01 18:38:09 +0300

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   def handle_routing_error
-    render json: { errorMessage: 'URL provided is not found please check your URL', status: 'failed' }, status: :not_found
+    render json: { errorMessage: 'URL provided is not found please check your URL', status: 'failed' },
+           status: :not_found
   end
 end
